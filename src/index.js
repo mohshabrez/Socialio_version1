5a09361944736073db9364bb5d8ec16e417537cd
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2023-06-30 18:09:48 +0530

    Push-Main

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+// import {makeServer} from "./server"
 import reportWebVitals from './reportWebVitals';
+import { MediaProvider } from './Context/MediaContext';
+import { AuthProvider } from './Context/AuthContext';
+import { BookProvider } from './Context/BookContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// makeServer();
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <MediaProvider>
+          <BookProvider>
+           <App />
+          </BookProvider>
+        </MediaProvider>
+      </AuthProvider>
+    </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
